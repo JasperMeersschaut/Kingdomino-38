a72db4b35ab4b21def0a7da4a6df61eb1c5ba16b
Jasper Meersschaut <jasper.meersschaut@student.hogent.be>
2024-03-12 20:44:04 +0100

    Update SpelMenu
    Code opgesplitst in methodes
    Kleurenlijst toegevoegd

--- src/cui/SpelMenu.java
+++ src/cui/SpelMenu.java
@@ -1,49 +1,67 @@
 package cui;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import DTO.AlleSpelersDTO;
 import DTO.SpelerDTO;
 import domein.DomeinController;
 import utils.Kleur;
 
-import java.util.List;
-import java.util.Scanner;
-import java.util.ArrayList;
-
 public class SpelMenu {
-    List<SpelerDTO> spelers = new ArrayList<>();
-    DomeinController dc = new DomeinController();
-    Scanner scanner = new Scanner(System.in);
+	List<SpelerDTO> spelers = new ArrayList<>();
+	DomeinController dc = new DomeinController();
+	List<AlleSpelersDTO> alleSpelers = dc.toonBeschikbareSpelers();
 
-    public void spelerToevoegen() {
-        int i = 0;
-        String naam;
-        Kleur kleur;
-        toonBeschikbareSpelers();
-        do {
-            i++;
-            System.out.println("Geef de naam van de speler die je wilt toevoegen: ");
-            naam = scanner.nextLine();
-            System.out.println("Geef de kleur van de speler die je wilt toevoegen: ");
-            kleur = Kleur.valueOf(scanner.nextLine());
-            spelers.add(new SpelerDTO(naam, kleur));
-            dc.toonBeschikbareSpelers().remove(naam);
-            if (i < 4 && i > 2) {
-                System.out.println("Wil je nog een speler toevoegen? (ja/nee)");
-                String antwoord = scanner.nextLine();
-                if (antwoord.equals("nee")) {
-                    i = 4;
-                }
-            }
-        } while (i >= 4);
-      dc.startSpel(spelers);
-    }
+	Scanner scanner = new Scanner(System.in);
 
+	public String getSpelerNaam() {
+		System.out.print("Geef de naam van de speler die je wilt toevoegen: ");
 
-    private void toonBeschikbareSpelers() {
-        System.out.println("Beschikbare bestaande speler spelers: ");
-        List<AlleSpelersDTO> spelers = dc.toonBeschikbareSpelers();
-        for (AlleSpelersDTO speler : spelers) {
-            System.out.println(speler.gebruikersnaam());
-        }
-    }
+		String naam = scanner.nextLine();
+		alleSpelers.removeIf(speler -> speler.gebruikersnaam().equals(naam));
+		return naam;
+	}
+
+	public Kleur getSpelerKleur(List<Kleur> beschikbareKleuren) {
+		System.out.println("Beschikbare kleuren: " + beschikbareKleuren);
+		System.out.print("Geef de kleur van de speler die je wilt toevoegen: ");
+		Kleur kleur = Kleur.valueOf(scanner.nextLine().toUpperCase());
+		beschikbareKleuren.remove(kleur);
+		return kleur;
+	}
+
+	public void voegSpelerToe(String naam, Kleur kleur) {
+		spelers.add(new SpelerDTO(naam, kleur));
+	}
+
+	public boolean vraagNieuweSpeler(int i) {
+		if (i < 4 && i > 2) {
+			System.out.println("Wil je nog een speler toevoegen? (ja/nee)");
+			String antwoord = scanner.nextLine();
+			return !antwoord.equals("nee");
+		}
+		return true;
+	}
+
+	public void spelerToevoegen() {
+		int i = 0;
+		List<Kleur> beschikbareKleuren = Kleur.geefKleuren();
+		do {
+			i++;
+			toonBeschikbareSpelers();
+			String naam = getSpelerNaam();
+			Kleur kleur = getSpelerKleur(beschikbareKleuren);
+			voegSpelerToe(naam, kleur);
+		} while (vraagNieuweSpeler(i) && i <= 4);
+		dc.startSpel(spelers);
+	}
+
+	private void toonBeschikbareSpelers() {
+		System.out.println("Beschikbare bestaande speler spelers: ");
+		for (AlleSpelersDTO speler : alleSpelers) {
+			System.out.println(speler.gebruikersnaam());
+		}
+	}
 }
